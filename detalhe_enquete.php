03da3f3df994b6f9f7b013defbf6e746f008ae09
Aquiles Burlamaqui <aquilesburlamaqui@gmail.com>
2015-03-26 11:44:39 -0300

    realizando update nos votos

--- detalhe_enquete.php
+++ detalhe_enquete.php
@@ -9,10 +9,14 @@
 		$query = "SELECT * FROM pergunta where id_enquete='$_GET[id]'";
 		$result = mysql_query($query, $con);
 		echo $_GET[titulo]."</br>";
+		echo "<form action='votar_enquete.php' method='POST'>";
 		while($linha = mysql_fetch_array($result)) {
 			
-			echo $linha["texto"]."</br>";
+			echo "<input type='radio' name='enquete' value='".$linha["id"]."'>".$linha["texto"]."</br>";
 		}
+                  echo "<input type='hidden' name='id_enquete' value='".$_GET[id]."'/>";  
+                  echo "<input type='submit' value='Votar'/>";   
+                  echo "</form>"; 
 		?>
 		
 		<p><a href="index.php">Home</a></p>
--- listar_enquete.php
+++ listar_enquete.php
@@ -9,7 +9,9 @@
 		$query = "SELECT * FROM enquete";
 		$result = mysql_query($query, $con);
 		while($linha = mysql_fetch_array($result)) {
-		   echo "<a href='detalhe_enquete.php?id=".$linha["id"]."&titulo=".$linha["titulo"]."'>id: ".$linha["id"]." - "."titulo: ".$linha["titulo"]."</a></br>";
+		   echo "<a href='detalhe_enquete.php?id=".$linha["id"]."&titulo=".$linha["titulo"]."'>id: ".$linha["id"]." - "."titulo: ".$linha["titulo"]."</a> - ";
+
+echo "  <a href='resultado_enquete.php?id=".$linha["id"]."&titulo=".$linha["titulo"]."'>Resultado</a></br>";
 		}
 		?>
 		
