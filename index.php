3768928eca754451905b39fa9b21f9dce25f7e62
Aquiles Burlamaqui <aquilesburlamaqui@gmail.com>
2015-03-26 01:03:37 -0300

    login criado

--- index.php
+++ index.php
@@ -1,9 +1,14 @@
 <html>
 	<head>
-		<title>Aquiles Home Page</title>
+		<title>Quiz ClassRoom Service</title>
 	</head>
 	<body>
+		<form action="login.php" method="POST">  
+                   E-mail:<input type="text" name="email"/></br>
+                   Senha:<input type="password" name="senha"/>     
+		   <input type="submit" value="OK"/>                   
+		</form>  	
 		<a href="cadastro.php">Cadastro</a>
-		<a href="listar.php">Listar</a>
+		
 	</body>
 </html>
---
+++ login.php
@@ -0,0 +1,32 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+		<?php
+                 
+		$con = mysql_connect("localhost","root","root");
+		mysql_select_db("web1", $con);
+		$query = "SELECT * FROM usuario WHERE email='$_POST[email]'";
+		$result = mysql_query($query, $con);
+                $logado = 0;
+		while($linha = mysql_fetch_array($result)) {
+
+			if($linha["senha"]==md5($_POST[senha])) {
+                             echo "e-mail:".$linha["email"]."</br>";
+		             $logado = 1;
+			     break;
+                        }
+		}
+                if($logado==1)  {
+                     echo "Bem vindo!!!</br>";
+		     echo "<a href='listar.php'>Listar Usuarios</a></br>";
+                     echo "<a href='cadastro_enquete.php'>Criar Enquete</a></br>";
+		}
+		else
+                     echo "Senha ou usuario invalido!!!</br>";
+		?>
+		
+		<p><a href="index.php">Home</a></p>
+	</body>
+</html>
